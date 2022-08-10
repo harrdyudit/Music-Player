4f0c8aba6c5bbd0e1fc95e2903d5f5a0ae645ad6
udit <sharmaudit262@gmail,com>
2022-08-10 13:52:39 +0530

    "final update"

--- script.js
+++ script.js
@@ -1,78 +1,138 @@
-const music=document.querySelector(".aud")
-const img=document.getElementById("ig")
-const playd=document.getElementById("playm");
-const title=document.getElementById("title");
-const artist=document.getElementById("artist");
-const prew=document.getElementById("prev");
-const next=document.getElementById("forward");
-const songs=[
+const music = document.querySelector(".aud")
+const img = document.getElementById("ig")
+const playd = document.getElementById("playm");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const prew = document.getElementById("prev");
+const next = document.getElementById("forward");
+const progress = document.getElementById("progress");
+const current_time = document.getElementById("current_time");
+let total_duration = document.getElementById("duration");
+let progress_div=document.querySelector(".progress_div");
+const songs = [
     {
-    name:"Asal-1",
-    title:"Asal main",
-    artist:"Darshan Raval",
-    
-    
-},
+        name: "Asal-1",
+        title: "Asal main",
+        artist: "Darshan Raval",
+
+
+    },
     {
-    name:"Dippam-2",
-    title:"Dippam",
-    artist:"Anirudh",
-   
-    
-},
+        name: "Dippam-2",
+        title: "Dippam",
+        artist: "Anirudh",
+
+
+    },
     {
-    name:"Tu Mileya-3",
-    title:"Tu Mileya",
-    artist:"Darshan Raval",
+        name: "Tu Mileya-3",
+        title: "Tu Mileya",
+        artist: "Darshan Raval",
     }
-    
+
 ];
 
-let isPlaying=false;
+let isPlaying = false;
 
-const playMusic=()=>{
-    isPlaying=true;
+const playMusic = () => {
+    isPlaying = true;
     music.play();
-    playd.classList.replace("fa-circle-play","fa-circle-pause");
+    playd.classList.replace("fa-circle-play", "fa-circle-pause");
     img.classList.add("animate");
 
 
 };
-const pauseMusic=()=>{
-    isPlaying=false
+const pauseMusic = () => {
+    isPlaying = false
     music.pause();
-    playd.classList.replace("fa-circle-pause","fa-circle-play");
+    playd.classList.replace("fa-circle-pause", "fa-circle-play");
     img.classList.remove("animate");
 
 
 };
 
-playd.addEventListener("click",()=>{
-  isPlaying ? pauseMusic():playMusic();
+playd.addEventListener("click", () => {
+
+    isPlaying ? pauseMusic() : playMusic();
 });
 
-const loadSong=(songs)=>{
-    title.textContent=songs.title
-artist.textContent=songs.artist;
-music.src=`music/${songs.name}.mp3`
-img.src=`image/${songs.name}.jpg`
+const loadSong = (songs) => {
+    title.textContent = songs.title
+    artist.textContent = songs.artist;
+    music.src = `music/${songs.name}.mp3`
+    img.src = `image/${songs.name}.jpg`
 }
-songIndex=0;
+songIndex = 0;
 // loadSong(songs[2]);
 
-const nextSong=()=>{
-    songIndex=(songIndex+1) % songs.length
-  loadSong(songs[songIndex]);
-   
+const nextSong = () => {
+    songIndex = (songIndex + 1) % songs.length
+    loadSong(songs[songIndex]);
+
     playMusic();
-    
+
 }
-const prewSong=()=>{
-    songIndex=(songIndex-1+songs.length) % songs.length;
+const prewSong = () => {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
     loadSong(songs[songIndex]);
     playMusic();
-}
+};
+
+// progress meter 
+
+music.addEventListener("timeupdate", (ev) => {
+
+    const { currentTime, duration } = ev.target;
+    // formula for get progress time
+    let progress_time = (currentTime / duration) * 100;
 
 
-next.addEventListener("click",nextSong)
-prew.addEventListener("click",prewSong)
+    progress.style.width = `${progress_time}%`
+
+    // music update duration
+
+    // convert second into minute
+
+
+    // curreny=t duration update
+    const min_duration = Math.floor(duration / 60);
+    // find second
+    const sec_duration = Math.floor(duration % 60);
+
+
+
+    let updateduration = `${min_duration}:${sec_duration}`;
+
+
+    total_duration.textContent = `${updateduration}`
+
+    // current time update duration
+    let min_currentTime=Math.floor(currentTime/60)
+    let sec_currentTime=Math.floor(currentTime%60)
+
+    if(sec_currentTime<10){
+        sec_currentTime=`0${sec_currentTime}`
+    }
+    let currentTimeUpdate=`${min_currentTime}:${sec_currentTime}`;
+    current_time.textContent=`${currentTimeUpdate}`;
+});
+// progress-div when user click on progress bar and change song duration
+progress_div.addEventListener("click",(event)=>{
+    console.log(event);
+const {duration}=music;
+    let move_progress=(event.offsetX/event.target.clientWidth)*duration;
+    // console.log(duration);
+    // console.log(move_progress);
+    music.currentTime=move_progress;
+
+})
+
+// event for when the current song end the next song will automatically play
+music.addEventListener("ended",nextSong)
+
+
+next.addEventListener("click", nextSong)
+prew.addEventListener("click", prewSong)
